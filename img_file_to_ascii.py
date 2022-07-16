a589d243b7542a4340e11814c3c0b2720a2819bb
Jackboxx <lucywildhear0@gmail.com>
2022-07-16 14:21:19 +0200

    updates to video_to_ascii

---
+++ img_file_to_ascii.py
@@ -0,0 +1,44 @@
+from PIL import Image
+
+path = input('Enter an image path: ')
+img = Image.open(path)
+pixels = img.load()
+
+chunk_size_x = int(img.size[0] / 120);
+chunk_size_y = int(img.size[1] / 40);
+
+output = ""
+
+def assign_char(num):
+    opt = int(num / 32)
+    
+    match opt:
+        case 7:
+            return '#'
+        case 6:
+            return '@'
+        case 5:
+            return 'M'
+        case 4:
+            return '&'
+        case 3:
+            return 't'
+        case 2:
+            return '/'
+        case 1:
+            return '*'
+        case 0: 
+            return '\''
+
+for y in range(0, img.size[1] - chunk_size_y, chunk_size_y):
+    for x in range(0, img.size[0] - chunk_size_x, chunk_size_x):
+        sum = 0;
+        for i in range(0, chunk_size_x):
+            for j in range(0, chunk_size_y):
+                sum += pixels[x + i, y + j][0] + pixels[x + i, y + j][1] + pixels[x + i, y + j][2];
+    
+        sum /= (chunk_size_x*chunk_size_y*3)
+        output += assign_char(sum);
+    output += "\n";
+
+print(output)
